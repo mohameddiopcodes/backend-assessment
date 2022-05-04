dc8fc129e8bed1e7830017d0993752cbbf9368d5
Mohamed Diop <mohameddiop@Mohameds-MBP.lan>
2022-05-03 23:59:40 -0700

    full crud on example entity

--- src/api/controllers/example-entity/example-entity.ts
+++ src/api/controllers/example-entity/example-entity.ts
@@ -1,16 +1,32 @@
-import { Body, Get, JsonController, Post } from "routing-controllers";
+import { Body, Get, JsonController, Post, Put, Delete, Param } from "routing-controllers";
+import { DeleteResult, UpdateResult } from "typeorm";
 
 import { ExampleEntity } from "../../models";
 
 @JsonController("/example-entity")
 export class UserController {
   @Get()
-  get(): Promise<ExampleEntity[]> {
+  getAll(): Promise<ExampleEntity[]> {
     return ExampleEntity.find();
+  }
+
+  @Get("/:id")
+  getOne(@Param("id") id: string): Promise<ExampleEntity> {
+    return ExampleEntity.findOne(id);
   }
 
   @Post()
   create(@Body() body: Pick<ExampleEntity, "exampleColumn">): Promise<ExampleEntity> {
     return ExampleEntity.create(body).save();
+  }
+
+  @Put("/:id")
+  update(@Param("id") id: string, @Body() body: Pick<ExampleEntity, "exampleColumn">): Promise<UpdateResult> {
+    return ExampleEntity.update(id, {...body});
+  }
+
+  @Delete("/:id")
+  delete(@Param("id") id: string): Promise<DeleteResult> {
+    return ExampleEntity.delete(id);
   }
 }
